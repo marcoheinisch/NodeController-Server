a00e3dcd7147f702dd2d07bfa71fc20ce2f5414c
marcoheinisch <57726217+marcoheinisch@users.noreply.github.com>
2020-05-29 22:02:49 +0200

    inital commit

---
+++ CONFIGURATIONS.h
@@ -0,0 +1,5 @@
+//Network configuration
+#define WLANPASSWORD "password"  
+#define WLANSSID "ssid"                      
+#define IP { 000, 000, 000, 000 }             
+#define GATEWAY {000, 000, 000, 1 }         
